2e06c5b10db092d8dcea2d6f1397679c40ceb705
Haider <habbas33@gmail.com>
2022-08-30 21:35:59 +0500

    Update admin.py

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from .models.daos_models import *
+from .models.proposals_models import *
+from .models.vote_models import *
+
 
 # Register your models here.
 
 admin.site.register(Daos)
+admin.site.register(Proposal)
+admin.site.register(Vote)
